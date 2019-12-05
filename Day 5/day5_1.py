7a937649fe59643fb8b1f69d223802c3d9fa1873
deep-in-the-pc <1160640@isep.ipp.pt>
2019-12-05 19:24:14 +0000

    Added opcode 03 and 04

--- Day 5/day5_1.py
+++ Day 5/day5_1.py
@@ -1,35 +1,35 @@
 import math
 
-def parser(opcode_list_r):
+def parser(opcode_list_r, input_var):
     PC = 0
     opcode = opcode_list_r[PC]
+    output_var = None
     while(opcode != 99):
-        if opcode == 1:
+        if str(opcode)[-2:] == '01':
             #print(opcode_list[PC + 3], opcode_list[opcode_list[PC + 1]], opcode_list[opcode_list[PC + 2]])
             try:
                 opcode_list_r[opcode_list_r[PC + 3]] = opcode_list_r[opcode_list_r[PC+1]] + opcode_list_r[opcode_list_r[PC+2]]
                 PC = PC + 4
             except:
                 return [0]
-        if opcode == 2:
+        if str(opcode)[-2:] == '02':
             #print(opcode_list[PC + 3], opcode_list[opcode_list[PC+1]], opcode_list[opcode_list[PC+2]])
             try:
                 opcode_list_r[opcode_list_r[PC + 3]] = opcode_list_r[opcode_list_r[PC+1]] * opcode_list_r[opcode_list_r[PC+2]]
                 PC = PC + 4
             except:
                 return [0]
-        if opcode == 3:
+        if str(opcode)[-2:] == '03':
             #print(opcode_list[PC + 3], opcode_list[opcode_list[PC+1]], opcode_list[opcode_list[PC+2]])
             try:
-                opcode_list_r[opcode_list_r[PC + 3]] = opcode_list_r[opcode_list_r[PC+1]] * opcode_list_r[opcode_list_r[PC+2]]
-                PC = PC + 3
+                opcode_list_r[opcode_list_r[PC + 1]] = input_var
             except:
                 return [0]
-        if opcode == 3:
+        if str(opcode)[-2:] == '04':
             #print(opcode_list[PC + 3], opcode_list[opcode_list[PC+1]], opcode_list[opcode_list[PC+2]])
             try:
-                opcode_list_r[opcode_list_r[PC + 3]] = opcode_list_r[opcode_list_r[PC+1]] * opcode_list_r[opcode_list_r[PC+2]]
-                PC = PC + 3
+                output_var = opcode_list_r[opcode_list_r[PC + 1]]
+                print(output_var)
             except:
                 return [0]
         opcode = opcode_list_r[PC]
@@ -41,17 +41,8 @@ def main():
     with open("input2") as input:
         opcode_list = list(map(int, str(input.readline()).split(",")))
         size = len(opcode_list)
-    for i in range(0, size):
-        for n in range(0, size):
-            with open("input2") as input:
-                opcode_list = list(map(int, str(input.readline()).split(",")))
-                opcode_list[1] = n
-                opcode_list[2] = i
-                out = parser(opcode_list)
-                #print(n, i, out)
-                if out[0] == 19690720:
-                        print(100 * n + i)
-                        return 1
+    out = parser(opcode_list, 1)
+
 
 if __name__ == "__main__":
     main()
